05b1d8efcbe6de076c8bd0b07132c713ec1bd7cd
Mikkel Blankholm <mb@miraculous.dk>
2012-02-23 11:52:23 +0100

    Update lib/formtastic-bootstrap/inputs/base/wrapping.rb

--- lib/formtastic-bootstrap/inputs/base/wrapping.rb
+++ lib/formtastic-bootstrap/inputs/base/wrapping.rb
@@ -21,7 +21,7 @@ module FormtasticBootstrap
         end
 
         def input_div_wrapping(inline_or_block_errors = :inline)
-          template.content_tag(:div, :class => "input") do 
+          template.content_tag(:div, :class => "controls") do 
             [yield, error_html(inline_or_block_errors), hint_html(inline_or_block_errors)].join("\n").html_safe  
           end
         end
@@ -37,7 +37,7 @@ module FormtasticBootstrap
           opts[:class] ||= []
           opts[:class] = [opts[:class].to_s] unless opts[:class].is_a?(Array)
           opts[:class] << as
-          opts[:class] << "clearfix"
+          opts[:class] << "control-group"
           # opts[:class] << "input"
           opts[:class] << "error" if errors?
           opts[:class] << "optional" if optional?
